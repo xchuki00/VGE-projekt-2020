a189d67191f2b6924ee2bd8ddfc4d979597d80b7
Patrik Chukir <p.chukir@gmail.com>
2020-04-05 22:23:13 +0200

    add .gitignore

---
+++ include/OutputWindow.h
@@ -0,0 +1,15 @@
+//
+// Created by patrik on 05.04.20.
+//
+
+#ifndef VGE_OUTPUTWINDOW_H
+#define VGE_OUTPUTWINDOW_H
+
+
+class OutputWindow {
+    OutputWindow();
+
+};
+
+
+#endif //VGE_OUTPUTWINDOW_H
---
+++ src/VgeApp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by patrik on 05.04.20.
+//
+
+#include "VgeApp.h"
